36e569e50b7321aa19693caf89fbb3151dbd44c9
zhaiyb <zhaiyibo10@163.com>
2017-11-02 15:04:49 +0800

    BizWork

--- App.js
+++ App.js
@@ -5,37 +5,55 @@ import {
   Text,
   View
 } from 'react-native';
+import Main from './src/component/Main';
+import Login from './src/component/Login';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+  'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+  'Shake or press menu button for dev menu',
+});
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogin: false
+    };
+  }
+  componentWillMount() {
+    try {
+      //const token = await AsyncStorage.getItem('loginToken');
+      /*this.setState({
+        isLogin: !!token
+      });*/
+    } catch (error) {
+      // Error saving data
+    }
+  }
+  handleLogin = () => {
+    try {
+      //const token = await AsyncStorage.setItem('loginToken','test');
+      this.setState({
+        isLogin: !this.state.isLogin
+      });
+    } catch (error) {
+      // Error saving data
+    }
+  };
   render() {
+    const {isLogin} = this.state;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-      </View>
+      <Main/>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: '#F5FCFF'
+  }
 });
