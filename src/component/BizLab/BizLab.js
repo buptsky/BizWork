51f86678b5323819a4d002f1dfb119aaa67e23c5
zhaiyb <zhaiyibo10@163.com>
2017-11-09 17:40:31 +0800

    BizWork

--- src/component/BizLab/BizLab.js
+++ src/component/BizLab/BizLab.js
@@ -14,7 +14,7 @@ import FaceScan from './FaceScan';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import LinearGradient from 'react-native-linear-gradient';
 import fetchData from '../../util/fetchData';
-
+import  servers from '../../util/servers';
 export default class BizLab extends Component {
 
   static navigationOptions = ({navigation}) => {
@@ -32,7 +32,7 @@ export default class BizLab extends Component {
   async componentWillMount() {
     try {
       const res =  await fetchData({
-        url: '/queryUser.do',
+        url: servers.FaceScan + '/queryUser.do',
         data: {}
       });
       if (res.status) {
