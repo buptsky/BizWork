16a13150071ac1eec4d2e66af2f359a72622d241
zhaiyb <zhaiyibo10@163.com>
2017-11-10 11:26:43 +0800

    BizWork

--- src/util/servers.js
+++ src/util/servers.js
@@ -1,8 +1,10 @@
+const httpProtocol = 'http://';
 const servers = {
-  XuQiang: 'http://10.129.148.81:9000',
-  FaceScan: 'http://10.129.148.81:8585',
-  BizTask: 'api.task.bizwork.sogou',
-  BizTaskDev: '10.142.72.148:9000',
-  BizWork: 'api.bizwork.sogou'
+  //FaceScan: httpProtocol + '10.129.148.81:9000', //徐强
+  //FaceScan: httpProtocol + '10.129.148.81:8585',
+  FaceScan: httpProtocol + '10.142.82.212:9191',
+  BizTask: httpProtocol + 'api.task.bizwork.sogou',
+  BizTaskDev: httpProtocol + '10.142.72.148:9001',
+  BizWork: httpProtocol + 'api.bizwork.sogou'
 };
 export default servers;
